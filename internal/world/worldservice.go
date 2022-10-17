e89975802867f58c2c2ef4a02fd95f1da58e97d7
lukegriffith <lukemgriffith@gmail.com>
2022-10-17 20:57:05 +0100

    World statuses surfaced to ui

--- internal/world/worldservice.go
+++ internal/world/worldservice.go
@@ -2,18 +2,29 @@ package world
 
 import "errors"
 
-type worldService map[string]*World
+type worldService struct {
+	Worlds       map[string]*World
+	WorldsStatus map[string]Status
+}
 
 var (
 	Worlds worldService
 )
 
+func (w worldService) GetWorldStatuses() map[string]string {
+	statuses := make(map[string]string)
+	for k, v := range w.WorldsStatus {
+		statuses[k] = v.String()
+	}
+	return statuses
+}
+
 func NewWorldService() {
-	Worlds = make(worldService)
+	Worlds = worldService{make(map[string]*World), make(map[string]Status)}
 }
 
 func GetWorldBoard(name string, cycle int) (GridHistory, error) {
-	if world, ok := Worlds[name]; ok {
+	if world, ok := Worlds.Worlds[name]; ok {
 		grid := world.GetCycle(cycle)
 		return grid, nil
 	}
@@ -22,9 +33,21 @@ func GetWorldBoard(name string, cycle int) (GridHistory, error) {
 }
 
 func GetWorld(name string) World {
-	return *Worlds[name]
+	return *Worlds.Worlds[name]
 }
 
-func RegisterWorld(name string, w World) {
-	Worlds[name] = &w
+func RegisterWorld(name string) error {
+	if _, ok := Worlds.WorldsStatus[name]; !ok {
+		Worlds.WorldsStatus[name] = Pending
+		return nil
+	} else {
+		return errors.New("World is already registered")
+	}
+}
+
+func SetWorld(name string, w World) {
+	if _, ok := Worlds.WorldsStatus[name]; ok {
+		Worlds.Worlds[name] = &w
+		Worlds.WorldsStatus[name] = Ready
+	}
 }
