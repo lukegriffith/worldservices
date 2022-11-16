0657d0e6c2fe43b706cfbb79bb950b6c5293b26d
lukegriffith <lukemgriffith@gmail.com>
2022-11-16 20:04:56 +0000

    Adding a terminal UI to control the program

---
+++ cmd/terminal_ui/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"context"
+
+	"github.com/rivo/tview"
+)
+
+var (
+	ctx context.Context
+)
+
+func getWorlds(right *tview.List) {
+	worlds := []string{"world1", "world2"}
+
+	for i, world := range worlds {
+		worldText := world
+		right = right.AddItem(worldText, "Not Ready", rune(i+97), func() {
+			modal := tview.NewModal().
+				SetText(worldText).
+				AddButtons([]string{"Home"}).
+				SetDoneFunc(func(buttonIndex int, buttonLabel string) {
+					if buttonLabel == "Home" {
+						home()
+					}
+				})
+			app := ctx.Value("app").(*tview.Application)
+			app.SetRoot(modal, true)
+		})
+	}
+
+}
+
+func home() {
+	app := ctx.Value("app").(*tview.Application)
+	flex := ctx.Value("flex").(*tview.Flex)
+	app.SetRoot(flex, true)
+}
+
+func main() {
+
+	ctx = context.Background()
+	app := tview.NewApplication()
+	ctx = context.WithValue(ctx, "app", app)
+
+	newWorld := func() {
+		app := ctx.Value("app").(*tview.Application)
+		form := ctx.Value("form").(*tview.Form)
+		app.SetRoot(form, true)
+
+	}
+	refreshWorlds := func() {
+		right := ctx.Value("right").(*tview.List)
+		right.Clear()
+		getWorlds(right)
+	}
+	renderWorld := func() {
+		right := ctx.Value("right").(*tview.List)
+		app := ctx.Value("app").(*tview.Application)
+		app.SetFocus(right)
+	}
+	createWorld := func() {
+
+	}
+
+	form := tview.NewForm().
+		AddInputField("World name", "", 20, nil, nil).
+		AddInputField("Sim Pop", "", 3, tview.InputFieldInteger, nil).
+		AddInputField("Sim Size", "", 3, tview.InputFieldInteger, nil).
+		AddButton("Save", createWorld).
+		AddButton("Home", home)
+
+	ctx = context.WithValue(ctx, "form", form)
+
+	opList := tview.NewList().
+		AddItem("Create World", "Create a new creature simulation", 'a', newWorld).
+		AddItem("Refresh Worlds", "Refreshes world list", 'b', refreshWorlds).
+		AddItem("Render World", "Renders the simulation", 'c', renderWorld).
+		AddItem("Quit", "Press to exit", 'q', func() {
+			app := ctx.Value("app").(*tview.Application)
+			app.Stop()
+		})
+	ctx = context.WithValue(ctx, "opList", opList)
+
+	right := tview.NewList()
+	getWorlds(right)
+	ctx = context.WithValue(ctx, "right", right)
+	flex := tview.NewFlex().
+		AddItem(opList, 0, 1, true).
+		AddItem(right, 40, 1, true)
+	ctx = context.WithValue(ctx, "flex", flex)
+
+	if err := app.SetRoot(flex, true).EnableMouse(true).Run(); err != nil {
+		panic(err)
+	}
+
+}
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/faiface/pixel v0.10.0
 	github.com/jinzhu/copier v0.3.5
 	github.com/patrikeh/go-deep v0.0.0-20220129152125-82b8db494fe5
+	github.com/rivo/tview v0.0.0-20221115143349-ed3ea789e9f7
 	github.com/schwarmco/go-cartesian-product v0.0.0-20180515110546-d5ee747a6dc9
 	golang.org/x/image v0.1.0
 )
